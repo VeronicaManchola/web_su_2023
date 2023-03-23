71a6938236a344d66ab44e45f4a2bb3a465838c7
VeronicaManchola <veronica.manchola@gmail.com>
2023-03-22 21:58:32 -0300

    add missing images

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,16 @@ const areasSections = [
   { id: 'odontogeriatria', image: 'odontogeriatria.png', title: 'Odontogeriatría: Especialista en Adulto Mayor' },
   { id: 'protesis', image: 'protesis.png', title: 'Protesis Fija / Corona / Incrustación' },
   { id: 'implantes', image: 'implantes.png', title: 'Implantes Dentales' },
+  { id: 'ortodoncia', image: 'ortodoncia.png', title: 'Ortodoncia / Frenillos' },
+  { id: 'general', image: 'odontologia.png', title: 'Odontología General' },
+  { id: 'estetica', image: 'estetica.png', title: 'Estetíca Facial' },
+  { id: 'endodoncia', image: 'endodoncia.png', title: 'Endodoncia / Tratamiento de Conducto' },
+  { id: 'exodoncia', image: 'exodoncia.png', title: 'Exodoncia / Extracción de Dientes' },
+  { id: 'periodoncia', image: 'periodoncia.png', title: 'Periodoncia / Limpieza Dental' },
+  { id: 'blanqueamiento', image: 'blanqueamiento.png', title: 'Blanqueamiento Dental' },
+  { id: 'carillas', image: 'carillas.png', title: 'Carillas' },
+  { id: 'removible', image: 'removible.png', title: 'Reparación Removibles / Reparación' },
+  { id: 'bruxismo', image: 'bruxismo.png', title: 'Tratamiento de Bruxismo' },
 ];
 
 const Home: NextPage = () => {
